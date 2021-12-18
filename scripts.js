3ca1e3880ff84bd7f124fec6d2683b09136dc774
Yeaethwe <76844276+Yaethwe@users.noreply.github.com>
2021-12-18 21:22:38 +0630

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,86 @@
+//start phostDiv
+
+
+
+const petsData = [
+  {
+    name: "Purrsloud",
+    species: "Cat",
+    favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+    birthYear: 2016,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+  },
+  {
+    name: "Barksalot",
+    species: "Dog",
+    birthYear: 2008,
+    photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+  },
+  {
+    name: "Meowsalot",
+    species: "Cat",
+    favFoods: ["tuna", "catnip", "celery"],
+    birthYear: 2012,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+  }
+];
+
+function age(birthYear) {
+  let calculatedAge = new Date().getFullYear() - birthYear;
+  if (calculatedAge == 1) {
+    return "1 year old";
+  } else if (calculatedAge == 0) {
+    return "Baby";
+  } else {
+    return `${calculatedAge} years old`;
+  }
+}
+
+function foods(foods) {
+  return `
+<h4>Favorite Foods</h4>
+<ul class="foods-list">
+${foods.map(food => `<li>${food}</li>`).join("")}
+</ul>
+`;
+}
+
+function petTemplate(pet) {
+  return `
+    <div class="animal">
+    <img class="pet-photo" src="${pet.photo}">
+    <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
+    <p><strong>Age:</strong> ${age(pet.birthYear)}</p>
+    ${pet.favFoods ? foods(pet.favFoods) : ""}
+    </div>
+  `;
+}
+
+
+
+//endphost Div
+
+
+//noticbox's function
+/* 
+$(document).ready(function(){
+  $(".close").click(function(){
+    $("#myAlert").alert("close");
+  });
+});
+*/
+
+document.getElementById("app").innerHTML = `
+  <h1 class="app-title">Pets (${petsData.length} results)</h1>
+  ${petsData.map(petTemplate).join("")}
+  <p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates.</p>
+`;
+
 function aboutus() {
-	window.location.href="aboutus.html"
+	location.href="aboutus.html";
 }
 
 function home() {
-	window.location.href="index.html"
+	location.href="index.html";
 }
+
